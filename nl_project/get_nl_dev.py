aa11333b1c8fa7850748c30179234f89664faef2
Sean Granados <95511189+Upsilonlabsean@users.noreply.github.com>
2023-08-31 17:06:58 -0700

    added solution to unfound stars in Grant's work

--- nl_project/get_nl_dev.py
+++ nl_project/get_nl_dev.py
@@ -71,7 +71,7 @@ from astropy.wcs import WCS
 warnings.filterwarnings("ignore")
 
 # A:
-
+        
 epoch = input('epoch (e.g. 20160503): ')
 stf_ver = input('legacy or sinpsf: ')
 if stf_ver == 'legacy':
@@ -79,9 +79,52 @@ if stf_ver == 'legacy':
 if stf_ver == 'sinpsf':
     stf = 'v3_1'
     
-# Read in psf star list 
-psf_stf_loc = '/g/ghez/data/dr/{0}/source_list/psf_central.dat'.format('dr1')
+    
+# Function to identify stars not named in Grant's Work
+def find_stars(ind_lis, epoch, star):
+    # Steps:
+    # 1. Take average of the differences between ind_frame star locations and rms_named.lis star locations
+    # of known stars
+    # 2. Identify this average as an offset transformation from ind_frame star locations to rms_named.lis star locations
+    # 3. Apply transformation to Grant's work and match the location with the star name given in rms_named.lis
+    
+    # Read in rms_named.lis file, only keep named stars
+    rms_named_file = f'/g/ghez/data/dr/dr1/starlists/combo/{epoch}nirc2/starfinder_{stf}/mag{epoch}nirc2_kp_rms_named.lis'
+    rms_named_lis = Table.read(rms_named_file, format = 'ascii.commented_header', delimiter = '\s')
+    indices_to_remove = [index for index, value in enumerate(rms_named_lis['name']) if value.startswith('star')] 
+    rms_named_lis.remove_rows(indices_to_remove)
+    
+    # Read in ind_frame lis file, only keep named stars
+    indices_to_remove = [index for index, value in enumerate(ind_lis['col1']) if value.startswith('star')]
+    ind_lis.remove_rows(indices_to_remove)
+    
+    # Take difference between same stars in the two lists and average
+    rms_named_lis.sort('name')
+    ind_lis.sort('col1')
+    matched_stars = []
+    for s in ind_lis['col1']:
+        for n in rms_named_lis['name']:
+            if s == n:
+                matched_stars.append(s)
+            else:
+                continue
+    x_diff = []
+    y_diff = []
+    for ms in matched_stars:
+        guide_rms, guide_ind = np.where(rms_named_lis['name'] == ms)[0], np.where(ind_lis['col1'] == ms)[0]
+        x_diff.append(float(ind_lis['col4'][guide_ind] - rms_named_lis['x'][guide_rms]))
+        y_diff.append(float(ind_lis['col5'][guide_ind] - rms_named_lis['y'][guide_rms]))
+    x_rms_to_ind = np.mean(x_diff)
+    y_rms_to_ind = np.mean(y_diff)
+    guide = np.where(rms_named_lis['name'] == star)[0]
+    x_loc = float(rms_named_lis['x'][guide]) + x_rms_to_ind
+    y_loc = float(rms_named_lis['y'][guide]) + y_rms_to_ind
+    return x_loc, y_loc
 
+    
+# Read in psf star list 
+#psf_stf_loc = '/g/ghez/data/dr/{0}/source_list/psf_central.dat'.format('dr1')
+psf_stf_loc = '/u/ciurlo/Research/starfinder_tests/PSFstars_lists/25_PSFstars/psf_central.dat'
 psf_stf_lis = Table.read(psf_stf_loc, format = 'ascii.commented_header', delimiter = '\s')
 
 # Get list of PSF stars
@@ -143,13 +186,18 @@ match = (glob.glob(ind_loc))[0]
 ind_lis = Table.read(match, format = 'ascii.no_header', delimiter = '\s')
 star_x = []
 star_y = []
-for star in psf_stars:
+for j, star in enumerate(psf_stars):
     for i in range(len(ind_lis['col1'])):   
         if ind_lis['col1'][i] == star:
             star_x.append(ind_lis['col4'][i])
             star_y.append(ind_lis['col5'][i])
         else:
             continue
+    if (len(star_x) - 1) != j:
+        star_x.append(find_stars(ind_lis, epoch, star)[0])
+        star_y.append(find_stars(ind_lis, epoch, star)[1])
+    else:
+        continue
             
 for i in range(len(psf_stars)):
     star_loc.append((star_x[i], star_y[i]))
@@ -225,17 +273,23 @@ dat_f = pd.DataFrame({'PSF Stars': psf_stars, 'Max Counts/COADD': maxcounts,
 dat_f.to_csv('{0}/dev_{1}_{2}.csv'.format(os.getcwd(), epoch, stf_ver))
 subprocess.run(['open', '{0}/dev_{1}_{2}.csv'.format(os.getcwd(), epoch, stf_ver)], check=True)
 
+sns.set_style('darkgrid')
+plt.figure(figsize = (8,8))
+sns.histplot(data = dat_f, x = 'Deviation (%)')
+plt.title('{0} (Frame {2}) in {1}'.format(epoch, stf_ver, raw_frame))
+plt.savefig('{0}/dev_hist_{1}_{2}.jpg'.format(os.getcwd(), epoch, stf_ver), format = 'jpg')
+plt.show()
 
 
 # Plot 
 sns.set_style('darkgrid')
-plt.figure(figsize = (len(psf_stars), len(psf_stars)))
+plt.figure(figsize = (len(psf_stars)/1.7, len(psf_stars)/1.7))
 plt.plot(M, x, label = 'Metchev Poly')
 plt.plot(lin, x, label = 'Linear')
 sns.scatterplot(data = dat_f, x = 'x_axis', y = 'Max Counts/COADD', hue = 'PSF Stars')
-plt.xlabel('Actual Counts/COADD', fontsize = len(psf_stars))
-plt.ylabel('Theoretical Counts/COADD', fontsize = len(psf_stars))
-plt.title('{0} (Frame {2}) in {1}'.format(epoch, stf_ver, raw_frame), fontsize = len(psf_stars))
+plt.xlabel('Actual Counts/COADD', fontsize = len(psf_stars)/1.5)
+plt.ylabel('Theoretical Counts/COADD', fontsize = len(psf_stars)/1.5)
+plt.title('{0} (Frame {2}) in {1}'.format(epoch, stf_ver, raw_frame), fontsize = len(psf_stars)/1.5)
 plt.legend()
 plt.savefig('{0}/dev_{1}_{2}.jpg'.format(os.getcwd(), epoch, stf_ver), format = 'jpg')
 plt.show()
